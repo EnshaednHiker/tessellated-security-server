34109c1ebc7cbbf9e01447bf00382e96e6fb4511
EnshaednHiker <nbingham09@gmail.com>
2017-06-03 18:46:59 -0500

    disabled transporter tester

--- router.js
+++ router.js
@@ -22,7 +22,7 @@ let transporter = nodemailer.createTransport({
   logger: true,
   debug: true
 });
-
+/*
 //verifies connection configuration works
 transporter.verify(function(error, success){
   if (error){
@@ -32,7 +32,7 @@ transporter.verify(function(error, success){
     console.log("Transporter success: ", success);
   }
 });
-
+*/
 //put this in server, placed before pulling in or declaring any routes to all cross origin requests
 router.all('*', function(req, res, next) {
     res.header('Access-Control-Allow-Credentials', true);
